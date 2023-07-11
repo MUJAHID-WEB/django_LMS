f545fcf908dbba64ffa07bd31fb75e3c94c7d298
mujahid <mujahid30390@gmail.com>
2023-07-11 21:13:33 +0600

    fine

--- educamy/settings.py
+++ educamy/settings.py
@@ -134,6 +134,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mujahid30390@gmail.com'
-EMAIL_HOST_PASSWORD = 'LI@Rbird07'
+EMAIL_HOST_PASSWORD = 'LI@Rbird077'
 
 
